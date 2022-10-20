32e134e210295392aa5073887406929c1c0fe8a5
Matthew Leland <mleland18@apu.edu>
2022-10-20 10:30:13 -0700

    upgrade: mvp

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -4,48 +4,95 @@ import yelp from '../api/yelp'
 
 const ResultsShowScreen = ({ navigation }) => {
   const [result, setResult] = useState()
+  const [reviews, setReviews] = useState([])
   const id = navigation.getParam('id')
 
-  const getResult = async (id) => {
-    const response = await yelp.get(`/${id}`)
-    setResult(response.data)
+  const getResult = async id => {
+    const rslt = await yelp.get(`/${id}`)
+    setResult(rslt.data)
+  }
+
+  const getReviews = async id => {
+    const rvs = await yelp.get(`/${id}/reviews`).then(response => {
+      setReviews(response.data.reviews)
+    })
   }
 
   useEffect(() => {
     getResult(id)
+    getReviews(id)
   }, [])
 
   if (!result) {
     return null
   }
 
-  return <View>
-    <Text style={styles.name}>{result.name}</Text>
-    <FlatList
-      horizontal
-      data={result.photos}
-      keyExtractor={photo => photo}
-      renderItem={({ item }) => {
-        return <Image style={styles.image} source={{ uri: item }}></Image>
-      }}
-    />
-    <Text style={styles.detail}>{result.display_phone}</Text>
-    <Text style={styles.detail}>{result.location.display_address}</Text>
-  </View>
+  if (!reviews) {
+    return null
+  }
+
+  const userOne = reviews[0].user.name + ' (' + reviews[0].rating + '/5)'
+  const textOne = reviews[0].text
+  const userTwo = reviews[1].user.name + ' (' + reviews[1].rating + '/5)'
+  const textTwo = reviews[1].text
+  const userThree = reviews[2].user.name + ' (' + reviews[2].rating + '/5)'
+  const textThree = reviews[2].text
+
+  return (
+    <View style={styles.all}>
+      <Text style={styles.name}>{result.name}</Text>
+      <FlatList
+        horizontal
+        data={result.photos}
+        keyExtractor={photo => photo}
+        renderItem={({ item }) => {
+          return (
+            <Image
+              style={styles.image}
+              source={{ uri: item }}
+            ></Image>
+          )
+        }}
+      />
+      <Text style={styles.user}>{userOne}</Text>
+      <Text style={styles.review}>{textOne}</Text>
+      <Text style={styles.user}>{userTwo}</Text>
+      <Text style={styles.review}>{textTwo}</Text>
+      <Text style={styles.user}>{userThree}</Text>
+      <Text style={styles.review}>{textThree}</Text>
+      <Text style={styles.detail}>Phone: {result.display_phone}</Text>
+      <Text style={styles.detail}>
+        Address: {result.location.display_address}
+      </Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
+  all: {
+    marginVertical: 10,
+  },
   name: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginHorizontal: 10,
   },
   detail: {
-    fontSize: 15
+    fontSize: 15,
+    margin: 10,
   },
   image: {
     height: 200,
     width: 300,
-    marginHorizontal: 15
+    margin: 10,
+  },
+  review: {
+    marginHorizontal: 10,
+  },
+  user: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginHorizontal: 10,
   },
 })
 
