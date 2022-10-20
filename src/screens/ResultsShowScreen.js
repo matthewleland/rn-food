6783af38ae0013df29ca5e7d9b216b2e6fd4469e
Matthew Leland <mjleland4@gmail.com>
2022-10-20 11:41:04 -0700

    full credit

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -31,17 +31,14 @@ const ResultsShowScreen = ({ navigation }) => {
     return null
   }
 
-  reviews[0] ? console.log('review 0') : console.log('no review')
 
   // const userOne = reviews[0].user.name + ' (' + reviews[0].rating + '/5)'
-  // const textOne = reviews[0].text
   // const userTwo = reviews[1].user.name + ' (' + reviews[1].rating + '/5)'
-  // const textTwo = reviews[1].text
   // const userThree = reviews[2].user.name + ' (' + reviews[2].rating + '/5)'
-  // const textThree = reviews[2].text
-
-  reviews[0] ? console.log('review 0') : console.log('no review')
-
+  
+  const textOne = reviews[0].text
+  const textTwo = reviews[1].text
+  const textThree = reviews[2].text
   return (
     <View style={styles.all}>
       <Text style={styles.name}>{result.name}</Text>
@@ -58,12 +55,12 @@ const ResultsShowScreen = ({ navigation }) => {
           )
         }}
       />
+      <Text style={styles.user}>{textOne}</Text>
+      <Text style={styles.user}>{textTwo}</Text>
+      <Text style={styles.user}>{textThree}</Text>
       {/* <Text style={styles.user}>{userOne}</Text>
-      <Text style={styles.review}>{textOne}</Text>
       <Text style={styles.user}>{userTwo}</Text>
-      <Text style={styles.review}>{textTwo}</Text>
-      <Text style={styles.user}>{userThree}</Text>
-      <Text style={styles.review}>{textThree}</Text> */}
+      <Text style={styles.user}>{userThree}</Text> */}
       <Text style={styles.detail}>Phone: {result.display_phone}</Text>
       <Text style={styles.detail}>
         Address: {result.location.display_address}
@@ -96,6 +93,7 @@ const styles = StyleSheet.create({
   user: {
     fontSize: 16,
     fontWeight: 'bold',
+    marginTop: 10,
     marginHorizontal: 10,
   },
 })
